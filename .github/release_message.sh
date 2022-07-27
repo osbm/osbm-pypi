571c0ea6914374d8ecda53676053b6288126a70c
osbm <osmanfbayram@gmail.com>
2022-07-28 01:51:23 +0300

    fix

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-previous_tag=$(git describe --tags --abbrev=0)
-echo "Release ${previous_tag} to "
-git log "${previous_tag}..HEAD" --oneline
+previous_tag=$(git tag --sort=-creatordate | sed -n 2p)
+git shortlog "${previous_tag}.." | sed 's/^./    &/'
 
