e0a861179e1bed2bc8e3d4eb34106e95f0bf05b5
osbm <osmanfbayram@gmail.com>
2022-07-28 01:33:24 +0300

    test

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 previous_tag=$(git tag --sort=-creatordate | sed -n 2p)
 echo "Release ${previous_tag} to "
-git log "${previous_tag}..HEAD" --oneline > release_message.md
+git log "${previous_tag}..HEAD" --oneline
 
--- osbm/__init__.py
+++ osbm/__init__.py
@@ -1,7 +1,15 @@
+__version__ = open("VERSION").read()
 
 
-__version__ = open('VERSION').read()
+def poisson_disc_sampling(size=(1000, 1000), k=30, radius=10, seed=42):
+
+    raise NotImplementedError
 
 
-def add_number(a, b):
-    return a + b
+def download_gist(url: str = None):
+
+    assert url is not None, "Please provide an URL"
+
+
+def set_matplotlib_rc(setting: str ="notebook"):
+    raise NotImplementedError
