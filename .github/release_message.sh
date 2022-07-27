2485dc7dd79f0f886444c24c58edfafa1ed553ba
osbm <osmanfbayram@gmail.com>
2022-07-28 02:24:10 +0300

    dont include two changelogs

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-previous_tag=$(git tag --sort=-creatordate | sed -n 2p)
+previous_tag=$(git tag --sort=-creatordate | sed -n 1p)
 git shortlog "${previous_tag}.." | sed 's/^./    &/'
 
