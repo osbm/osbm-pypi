7f115f3ac7df3b19704994a45c1b1efc916d2267
osbm <osmanfbayram@gmail.com>
2022-07-28 01:47:11 +0300

    update

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-previous_tag=$(git tag --sort=-creatordate | sed -n 2p)
+previous_tag=$(git describe --tags --abbrev=0)
 echo "Release ${previous_tag} to "
 git log "${previous_tag}..HEAD" --oneline
 
