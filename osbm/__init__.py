04c65b301dbd301dd2de05612bbac9ae75c1b31c
osbm <osmanfbayram@gmail.com>
2022-07-31 15:56:30 +0300

    add functions

--- osbm/__init__.py
+++ osbm/__init__.py
@@ -1,7 +1,8 @@
 from . import utils
+import importlib
 
-module_path = __file__.split("__init__.py")[0]
-__version__ = open(module_path + "VERSION").read()
+module_path = __file__.split("__init__.py", maxsplit=1)[0]
+__version__ = open(module_path + "VERSION", encoding="utf-8").read().strip()
 
 
 def download_gist(url: str = None):
@@ -12,3 +13,33 @@ def download_gist(url: str = None):
 
 def set_matplotlib_rc(setting: str = "notebook"):
     raise NotImplementedError
+
+
+def is_colab():
+    raise NotImplementedError()
+
+
+def is_kaggle():
+    assert importlib.util.find_spec("kaggle")
+
+    raise NotImplementedError()
+
+
+def gpu_name():
+    raise NotImplementedError()
+
+
+def download_huggingface_repository():
+    raise NotImplementedError()
+
+def add_kaggle_token():
+    assert is_colab(), "This function is only available in Colab"
+    from google.colab import files
+
+    uploaded = files.upload()
+    # take uploaded file and and move it to ~/.kaggle/kaggle.json
+
+    for file_name in uploaded.keys():
+        if file_name.endswith(".json"):
+            files.copy(file_name, "~/.kaggle/kaggle.json")
+            break
